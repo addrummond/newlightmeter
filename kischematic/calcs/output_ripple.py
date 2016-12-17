4b525214a721f7a1f047ee5b432773bcfba58b37
Alex Drummond <a.d.drummond@gmail.com>
2016-12-17 15:47:54 +0000

    Remove LDO

---
+++ kischematic/calcs/output_ripple.py
@@ -0,0 +1,20 @@
+#
+# Calculations of output ripple voltage for LM3670#
+#
+
+import math
+
+max_output_current = 0.005
+freq = 1000000
+vout = 3.3
+vin = 5.5
+equiv_series_r = 0.000245
+output_cap = 1e-5
+
+input_ripple_current = (max_output_current * math.sqrt((vout/vin) * (1 - (vout/vin)))) / 2
+vppc = input_ripple_current/(freq*8*output_cap)
+ppesr = input_ripple_current * equiv_series_r
+pprms = math.sqrt(vppc*vppc + ppesr*ppesr)
+
+print(pprms)
+
