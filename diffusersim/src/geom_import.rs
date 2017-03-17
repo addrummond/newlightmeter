10b9c75d6bafd52f1de1efce4efa80849654fd57
Alex Drummond <a.d.drummond@gmail.com>
2017-03-17 16:15:15 +0000

    Parsing code now working a bit.

--- diffusersim/src/geom_import.rs
+++ diffusersim/src/geom_import.rs
@@ -2,6 +2,7 @@ use geom as g;
 use std::char;
 use std;
 
+#[derive(Debug)]
 pub struct ParseError {
     pub line: usize,
     pub col: usize,
@@ -13,8 +14,10 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
         Line
     }
     
+    #[derive(Debug)]
     enum St {
-        Initial,
+        SkipSpace,
+        LookForSegType,
         GetCoord
     }
 
@@ -27,12 +30,11 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
         col: usize,
         current_str: Vec<char>,
         current_coords: Vec<g::Scalar>,
-        current_segment_type: SegmentType,
-        segments: Vec<g::Segment>
+        current_segment_type: SegmentType
     }
 
     let mut state_ = State {
-        kind: St::Initial,
+        kind: St::SkipSpace,
         i: input.chars(),
         c: ' ',
         eof: false,
@@ -40,10 +42,11 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
         col: 0,
         current_str: Vec::new(),
         current_coords: Vec::new(),
-        current_segment_type: SegmentType::Line,
-        segments: Vec::new()
+        current_segment_type: SegmentType::Line
     };
     let state = &mut state_;
+
+    let mut segments: Vec<g::Segment> = Vec::new();
 
     fn next(state: &mut State) {
         if let Some(cc) = state.i.next() {
@@ -56,11 +59,40 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
                 state.col += 1;
             }
         }
+        else {
+            state.eof = true;
+        }
     }
 
-    fn skip_space(state: &mut State) {
+    fn skip_space(state: &mut State) -> bool {
+        let mut found_newline = false;
         while !state.eof && char::is_whitespace(state.c) {
+            if state.c == '\n' {
+                found_newline = true;
+            }
             next(state);
+        }
+        return found_newline;
+    }
+
+    fn add_segment(state: &mut State, segments: &mut Vec<g::Segment>) -> Option<ParseError> {
+        match state.current_segment_type {
+            SegmentType::Line => {
+                if state.current_coords.len() != 4 {
+                    return Some(ParseError {
+                        line: state.line,
+                        col: state.col,
+                        err: "Expected four coordinates for line".to_string()
+                    });
+                }
+
+                segments.push(g::Segment {
+                    p1: g::Point2::new(state.current_coords[0], state.current_coords[1]),
+                    p2: g::Point2::new(state.current_coords[2], state.current_coords[3])
+                });
+
+                None
+            }
         }
     }
 
@@ -72,26 +104,40 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
         })
     }
 
-    skip_space(state);
+    let mut second_eof = false;
     loop {
-        if (state.eof) {
-            match state.kind {
-                St::Initial => { return make_error(state, "Unexpected end of input."); }
-                St::GetCoord => { return make_error(state, "TODO REPLACE"); }
+        // States get one chance to look at the EOF value.
+        if state.eof {
+            if !second_eof {
+                second_eof = true;
+            }
+            else {
+                match state.kind {
+                    St::SkipSpace => {
+                        return Ok(segments);
+                    }
+                    St::LookForSegType => {
+                        return make_error(state, "Unexpected end of input");
+                    }
+                    St::GetCoord => {
+                        return make_error(state, "Unexpected end of input");
+                    }
+                }
             }
         }
 
         match state.kind {
-            St::Initial => {
+            St::SkipSpace => {
+                skip_space(state);
+                state.kind = St::LookForSegType;
+            },
+            St::LookForSegType => {
                 if char::is_alphanumeric(state.c) {
                     state.current_str.push(state.c);
-                    // TODO TODO INSERT CALL TO NEXT
-                    next(state);
                 }
-                else if (char::is_whitespace(state.c)) {
+                else if char::is_whitespace(state.c) {
                     let s: String = state.current_str.iter().cloned().collect();
-                    if (s == "lineseg") {
-                        skip_space(state);
+                    if s == "lineseg" {
                         state.current_str.clear();
                         state.current_segment_type = SegmentType::Line;
                         state.kind = St::GetCoord;
@@ -103,34 +149,52 @@ fn parse_to_segments(input: &str) -> Result<Vec<g::Segment>, ParseError> {
                 else {
                     return make_error(state, "Expecting element name");
                 }
+                next(state);
             },
             St::GetCoord => {
-                skip_space(state);
                 let c = state.c;
-                if c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
-                   c == '6' || c == '7' || c == '8' || c == '9' || c == '-' || c == 'e' || c == '.' {
+                if !state.eof &&
+                   (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
+                   c == '6' || c == '7' || c == '8' || c == '9' || c == '-' || c == 'e' || c == '.') {
                     state.current_str.push(c);
+                    next(state);
                 }
-                else if c == '\n' {
+                else if state.eof || char::is_whitespace(c) {
+                    let newline = skip_space(state);
+                    let c = state.c;
                     let ns: String = state.current_str.iter().cloned().collect();
                     match ns.as_str().parse::<f64>() {
                         Ok(n) => {
                             let nn = n as g::Scalar;
                             state.current_coords.push(nn);
-                            return make_error(state, "TEMP!");
+                            state.current_str.clear();
+                            if newline || state.eof {
+                                add_segment(state, &mut segments);
+                                state.current_coords.clear();
+                                state.kind = St::SkipSpace;
+                            }
                         },
                         Err(_) => {
                             return make_error(state, "Error parsing coordinate floating point value.");
                         }
                     }
                 }
-                else if char::is_whitespace(c) {
-                    state.current_coords.push(0.0);
-                    skip_space(state);
+                else {
+                    return make_error(state, "Unexpected character.");
                 }
             }
         }
     }
+}
 
-    make_error(state, "TODO RESULT")
+mod tests {
+    use geom_import;
+
+    #[test]
+    fn parse_to_segments_test1() {
+        let input = "lineseg 0.0 1.0 2.0 3.0";
+        println!("STARTING");
+        let r = geom_import::parse_to_segments(input);
+        println!("{:?}", r);
+    }
 }
