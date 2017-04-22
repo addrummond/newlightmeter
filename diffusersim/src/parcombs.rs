4e1ac4f1c6180a468e28ea0166c69f2b4671f878
Alex Drummond <a.d.drummond@gmail.com>
2017-04-22 22:11:03 +0100

    Minor.

--- diffusersim/src/parcombs.rs
+++ diffusersim/src/parcombs.rs
@@ -11,7 +11,7 @@
 //!
 //! IMHO the parser combinator approach is a bit clunky in
 //! Rust, and of little benefit in an imperative language that already has
-//! state and sequencing as primitive concepts and built-in support
+//! state and sequencing as primitive concepts and built-in syntactic support
 //! for the Option monad.
 
 use std::str;
